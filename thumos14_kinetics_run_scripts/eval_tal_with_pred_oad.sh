cfa80a4d77006d393ebe7ee6bdea92ead420c848
tuantng <tuantng>
2022-08-30 11:36:53 +0000

    update path

--- thumos14_kinetics_run_scripts/eval_tal_with_pred_oad.sh
+++ thumos14_kinetics_run_scripts/eval_tal_with_pred_oad.sh
@@ -1,6 +1,6 @@
 python tools/ontal_evaluator.py \
-    --all_probs_file "models/reproduce_sequence_thumos14_numlayer3_decoder_layer4_enc_stride_1_kv7/validation_results/test_0_all_probs.pkl" \
-    --ground_truth_cls "models/reproduce_sequence_thumos14_numlayer3_decoder_layer4_enc_stride_1_kv7/validation_results/test_0_ground_truth_cls.pkl" \
-    --video_names_file "models/reproduce_sequence_thumos14_numlayer3_decoder_layer4_enc_stride_1_kv7/validation_results/test_0_video_names.pkl" \
+    --all_probs_file "models/reproduce_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_all_probs.pkl" \
+    --ground_truth_cls "models/reproduce_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_ground_truth_cls.pkl" \
+    --video_names_file "models/reproduce_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_video_names.pkl" \
     --ambiguos_info "/data/thumos14_feat/ambiguous_dict.pkl" \
     --oad_gt_file "/data/thumos14_feat/thumos_from_oad_multi_classes.pkl"
